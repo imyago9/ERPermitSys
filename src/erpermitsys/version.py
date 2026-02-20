b1556e35597e0b7820eb307310f3363bb60882ac
yagof <yagofarhat555@gmail.com>
2026-02-19 20:38:16 -0500

    v0.0.10 - Bumped version.py

--- src/erpermitsys/version.py
+++ src/erpermitsys/version.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-APP_VERSION = "0.0.9"
+APP_VERSION = "0.0.10"
 GITHUB_RELEASE_REPO = "imyago9/ERPermitSys"
 GITHUB_RELEASE_ASSET_NAME = "erpermitsys-windows.zip"
